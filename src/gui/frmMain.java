ac9f8bb95e04d988a5e4abb32d8cfdb4421def82
gestevez <gestevez@CAECOPAZ02.caecopaz.mil.ar>
2017-09-18 14:36:00 -0300

    Fix Operator

--- src/gui/frmMain.java
+++ src/gui/frmMain.java
@@ -8,27 +8,36 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+
 import clases.calculadora;
 import clases.cronometro;
 import clases.ecuacion;
 import clases.numeros;
+
 import java.awt.Component;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class frmMain extends JFrame {
 	private static final long serialVersionUID = 1L;
 	public static JLabel lblTiempo = new JLabel("tiempo: ");
 	private JPanel Panel1;
-	private JTextField textField;
+	private JTextField txtFormula;
 	private cronometro crono = new cronometro();
+	private static int _puntos = 0;
+	String numRnd;
+	private static JLabel lblInfo = new JLabel();
+	private static JLabel lblPuntos = new JLabel();
+	private static boolean operaciones = false;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -69,111 +78,153 @@ public class frmMain extends JFrame {
 		Panel1.setLayout(null);
 
 		JLabel lblNumero = new JLabel("Numero: ");
+		lblNumero.setForeground(Color.RED);
 		lblNumero.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNumero.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNumero.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNumero.setBounds(0, 28, 436, 30);
 		Panel1.add(lblNumero);
 
-		JLabel lblNewLabel = new JLabel("*");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel.setBounds(92, 132, 35, 41);
-		Panel1.add(lblNewLabel);
-
-		JLabel lblNewLabel_1 = new JLabel("/");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel_1.setBounds(301, 135, 35, 35);
-		Panel1.add(lblNewLabel_1);
-
-		JLabel lblNewLabel_2 = new JLabel("+");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel_2.setBounds(240, 135, 35, 35);
-		Panel1.add(lblNewLabel_2);
-
-		JLabel lblNewLabel_3 = new JLabel("-");
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel_3.setBounds(156, 135, 35, 35);
-		Panel1.add(lblNewLabel_3);
-
-		lblTiempo.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				crono.setCero(true);
-			}
-		});
+		JLabel lblOperaciones = new JLabel("/");
+		lblOperaciones.setHorizontalAlignment(SwingConstants.CENTER);
+		lblOperaciones.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblOperaciones.setBounds(187, 132, 50, 35);
+		Panel1.add(lblOperaciones);
 		lblTiempo.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblTiempo.setBounds(0, 0, 143, 22);
 		Panel1.add(lblTiempo);
 
-		JLabel lblOperaciones = new JLabel("Operaciones");
-		lblOperaciones.setHorizontalAlignment(SwingConstants.CENTER);
-		lblOperaciones.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblOperaciones.setBounds(0, 69, 436, 52);
-		Panel1.add(lblOperaciones);
+		JLabel lblopDisponibles = new JLabel("Operaciones Disponibles");
+		lblopDisponibles.setHorizontalAlignment(SwingConstants.CENTER);
+		lblopDisponibles.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblopDisponibles.setBounds(0, 91, 436, 30);
+		Panel1.add(lblopDisponibles);
 
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		textField.setHorizontalAlignment(SwingConstants.RIGHT);
-		textField.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		textField.setText("0");
-		textField.setBounds(47, 184, 349, 40);
-		Panel1.add(textField);
-		textField.setColumns(10);
-
-		JLabel lblInfo = new JLabel("");
 		lblInfo.setForeground(Color.RED);
 		lblInfo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblInfo.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
-		lblInfo.setBounds(0, 338, 436, 52);
+		lblInfo.setBounds(0, 299, 436, 22);
 		Panel1.add(lblInfo);
+
+		txtFormula = new JTextField();
+
+		lblPuntos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPuntos.setForeground(Color.RED);
+		lblPuntos.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
+		lblPuntos.setBounds(0, 340, 436, 22);
+		Panel1.add(lblPuntos);
+		txtFormula.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtFormula.setHorizontalAlignment(SwingConstants.RIGHT);
+		txtFormula.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		txtFormula.setText("0");
+		txtFormula.setBounds(47, 184, 349, 40);
+		Panel1.add(txtFormula);
+		txtFormula.setColumns(10);
+		crono.setCero(true);
+		if (operaciones)
+			lblOperaciones.setText("/   -");
+		else
+			lblOperaciones.setText("*   +");
 
 		JButton btnCalcular = new JButton("Calcular");
 		btnCalcular.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
-		btnCalcular.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				// if (!(Pattern.matches("^[-+]?[0-9]+([-+*/]+[-+]?[0-9]+)*$",
-				// textField.getText()))) {
-				// lblInfo.setText("error de sintaxis");
-				// }
-				// else
-				// {
-				try {
-					calculadora cal = new calculadora();
-					ecuacion ecu = new ecuacion();
-					ecu.addInput(textField.getText());
-					cal.addInput(ecu.toPostfix());
-
-					lblInfo.setText(String.format("%.0f", cal.evaluar()));
-					if (lblNumero.getText().contains(lblInfo.getText())) {
-						lblInfo.setText("Bien!");
-						textField.setText("");
-
-					} else {
-						lblInfo.setText("La cuenta no dio");
-						textField.setText("");
-					}
-
-				} catch (Exception ex) {
-					lblInfo.setText("Error en sintaxis");
-					textField.setText("");
+		txtFormula.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					if (calcular()) {
+						numRnd = generarNumero() + "";
+						lblNumero.setText("Numero: " + numRnd);
+						if (operaciones)
+							lblOperaciones.setText("/   -");
+						else
+							lblOperaciones.setText("*   +");
+					} else
+						lblInfo.setText("La formula tiene que contener los operadores validos");
+					if (operaciones)
+						lblOperaciones.setText("/   -");
+					else
+						lblOperaciones.setText("*   +");
 				}
-				// }
 			}
 		});
-		btnCalcular.setBounds(156, 252, 108, 40);
+		btnCalcular.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (calcular()) {
+					numRnd = generarNumero() + "";
+					lblNumero.setText("Numero: " + numRnd);
+
+				} else
+					lblInfo.setText("La formula tiene que contener los operadores validos");
+				if (operaciones)
+					lblOperaciones.setText("/   -");
+				else
+					lblOperaciones.setText("*   +");
+			}
+
+		});
+		btnCalcular.setBounds(156, 248, 108, 40);
 		Panel1.add(btnCalcular);
-		String numRnd = generarNumero() + "";
-		numRnd = numRnd.replace('.', ',');
+		numRnd = generarNumero() + "";
 		lblNumero.setText("Numero: " + numRnd);
 
+	}
+
+	private boolean calcular() {
+		if (operaciones) {
+			if (txtFormula.getText().contains("/")
+					&& txtFormula.getText().contains("-")
+					&& !txtFormula.getText().contains("+")
+					&& !txtFormula.getText().contains("*"))
+				operaciones = !operaciones;
+			else
+				return false;
+		} else {
+			if (txtFormula.getText().contains("*")
+					&& txtFormula.getText().contains("+")
+					&& !txtFormula.getText().contains("-")
+					&& !txtFormula.getText().contains("/"))
+				operaciones = !operaciones;
+			else
+				return false;
+		}
+
+		try {
+			calculadora cal = new calculadora();
+			ecuacion ecu = new ecuacion();
+			ecu.addInput(txtFormula.getText());
+			cal.addInput(ecu.analizarFormula());
+			String resultado = String.format("%.0f", cal.evaluar());
+
+			lblInfo.setText(resultado);
+			if (numRnd.equals(resultado)) {
+				lblInfo.setText("Tu Resultado:" + resultado + " ¡Bien!");
+				_puntos++;
+				lblPuntos.setText("Puntos: " + _puntos);
+				txtFormula.setText("");
+				txtFormula.requestFocus();
+
+			} else {
+				_puntos--;
+				lblInfo.setText("Tu Resultado:" + resultado
+						+ " La cuenta no dio");
+				lblPuntos.setText("Puntos: " + _puntos);
+				txtFormula.setText("");
+
+			}
+
+		} catch (Exception ex) {
+			lblInfo.setText("Error en sintaxis");
+			txtFormula.requestFocus();
+			return false;
+		}
+
+		return true;
 	}
 
 	private int generarNumero() {
 		numeros n = new numeros();
 		return n.get_numero();
+
 	}
 }
